6982ccca6ae93830edb73fb38ca9ab11a5abe2a9
Paarth1011 <paarthdalvi1011@gmail.com>
2022-08-15 00:16:30 +0530

    Reddot

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,26 @@
+// Script For 'Go To Top' Button
+window.onscroll = function()
+{
+    scrollFunction()
+};
+function scrollFunction() 
+{
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+    {
+        document.getElementById("myBtn").style.display = "block";
+    } 
+    else 
+    {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() 
+{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 // Script For Responsive Navbar
 
 function sideNavTrigger()
